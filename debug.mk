1ab3825f7374d3ad323d256fd2c95f7b338e4423
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-02 15:49:52 +1100

    Add main.cpp with Makefile

---
+++ debug.mk
@@ -0,0 +1,29 @@
+CC = g++
+CCFLAGS = -g -c -Wall
+
+LINKER = g++
+LINKERFLAGS = -Wall -o
+
+PROG = target
+
+SRCDIR = src
+BINARYDIR = bin
+
+OBJS = $(patsubst $(SRCDIR)/%.cpp, $(BINARYDIR)/%.o, $(wildcard $(SRCDIR)/*.cpp))
+
+$(BINARYDIR):
+	mkdir $(BINARYDIR)
+
+$(PROG): $(OBJS)
+	$(LINKER) $(OBJS) $(LINKERFLAGS) $(PROG)
+
+$(BINARYDIR)/%.o: $(SRCDIR)/%.cpp
+	$(CC) $(CCFLAGS) $< -o $@
+
+all: $(BINARYDIR) $(PROG)
+
+clean:
+	rm -rf *.o $(PROG) $(BINARYDIR)
+
+.PHONY: all clean
+.DEFAULT_GOAL = all
