c733e18cd893e5836229260372f23d611c38f8e7
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-02 19:41:44 +1100

    Make code better with goto

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,23 @@
 
 int main(int argc, char const *argv[]) {
 
+    int exit_code = 0;
+
     std::vector<std::string> args_vec(argv, argv + argc);
 
     args::Result* args_result = args::build(args_vec);
 
     if (!args_result->success) {
         std::cout << color::format_color(color::format_color("error", color::Color::FOREGROUND_LIGHT_RED), color::Color::BOLD) << ": Cannot parse args" << std::endl;
-        return 1;
+        exit_code = 1;
+        goto exit_args;
     }
 
     std::cout << args_result->data->get_file_name() << '\n';
 
-    delete args_result->data;
-    delete args_result;
+    exit_args:
+        delete args_result->data;
+        delete args_result;
 
-    return 0;
+        return exit_code;
 }
