82db1d784238428005ded917738dccc17989a28b
aixoio <79969530+aixoio@users.noreply.github.com>
2023-12-05 18:49:33 +1100

    Update printing

--- src/execute.cpp
+++ src/execute.cpp
@@ -3,6 +3,7 @@
 #include <map>
 #include <iostream>
 #include <cmath>
+#include <iomanip>
 #include "include/parser.hpp"
 #include "include/execute.hpp"
 
@@ -295,7 +296,7 @@ bool execute::execute(std::vector<parser::ParsedToken*> *tokens) {
 
                     if (next_token->get_token_type() == parser::ParsedTokenType::VAR) {
                         std::string key = ((parser::VarParsedToken*) next_token)->get_name();
-                        std::cout << key << " = " << vars->at(key) << std::endl;
+                        std::cout << key << " = " << std::setprecision(16) << vars->at(key) << std::endl;
                     } else {
                         delete vars;
                         return false;
